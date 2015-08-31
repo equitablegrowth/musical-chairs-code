555ba02fcf875b12cfc1240b8733d2f7ac95ec17
Austin Clemens <austinc@mac.com>
2015-08-31 11:40:27 -0400

    Graph data sets
    Added graph data sets and some edits to code

--- skilled.py
+++ skilled.py
@@ -12,8 +12,8 @@ import csv
 
 # A complete workflow might look like this:
 #	data=load_data(file)
-#	skilldata=skilled_graph(data)
-#	final=skilled_graph2(skilldata,dates=[2008,2014,2])
+#	educdata=education_graph(data)
+#	final=education_graph2(educdata,dates=[2008,2014,2])
 
 file='/qwi_ALL_se_f_n3_op_u.csv'
 
@@ -34,13 +34,13 @@ def load_data(file):
 	return data
 
 
-def skilled_graph(data):
+def education_graph(data):
 	# Take data from load_data and consolidate into national data rows. Currently data is by state
 	# and education level. This function collapses all states and all educations into a year/quarter
-	# by industry observations. This is where 'skilled' is defined as % of employees with BAs over
-	# total employees. Average wage is also calculated here.
+	# by industry observations. Calculate the share of workers with college degrees as % of employees 
+	# with BAs over total employees (not counting those <24). Average wage is also calculated here.
 
-	# This function is for the all employees graphs. For the hires graphs, the function skilled_graph
+	# This function is for the all employees graphs. For the hires graphs, the function education_graph
 	# _hires should be used instead.
 
 	# Load into pandas.
@@ -76,24 +76,24 @@ def skilled_graph(data):
 					e4_emp=quarter_data[quarter_data['education']=='E4']['EmpS'].sum()
 					total_earn=quarter_data['weighted'].sum()
 					average_wage=total_earn/total_emp
-					skilled_share=e4_emp/total_emp
+					educ_share=e4_emp/total_emp
 				except:
 					average_wage=0
-					skilled_share=0
+					educ_share=0
 					total_emp=0
 
-				national_list.append([year,quarter,industry,average_wage,skilled_share,total_emp])
+				national_list.append([year,quarter,industry,average_wage,educ_share,total_emp])
 
 	return national_list
 
 
-def skilled_graph_hires(datafile):
+def education_graph_hires(datafile):
 	# Take data from load_data and consolidate into national data rows. Currently data is by state
 	# and education level. This function collapses all states and all educations into a year/quarter
-	# by industry observations. This is where 'skilled' is defined as % of employees with BAs over
-	# total employees. Average wage is also calculated here.
+	# by industry observations. Calculate the share of workers with college degrees as % of employees 
+	# with BAs over total employees (not counting those <24). Average wage is also calculated here.
 
-	# This function is for the hires graphs. For the all employees graphs, the function skilled_graph
+	# This function is for the hires graphs. For the all employees graphs, the function education_graph
 	# should be used instead.
 
 	# Load into pandas.
@@ -129,52 +129,52 @@ def skilled_graph_hires(datafile):
 					e4_emp=quarter_data[quarter_data['education']=='E4']['HirAS'].sum()
 					total_earn=quarter_data['weighted'].sum()
 					average_wage=total_earn/total_emp
-					skilled_share=e4_emp/total_emp
+					educ_share=e4_emp/total_emp
 				except:
 					average_wage=0
-					skilled_share=0
+					educ_share=0
 					total_emp=0
 
-				national_list.append([year,quarter,industry,average_wage,skilled_share,total_emp])
+				national_list.append([year,quarter,industry,average_wage,educ_share,total_emp])
 
 	return national_list
 
 
-def skilled_graph2(skill_data,dates=[2008,2014,2]):
+def education_graph2(educ_data,dates=[2008,2014,2]):
 	# Finally combine start and end observations to get the change between two dates and output a
 	# single row for each industry that is suitable for creating the scatter plot in the research
-	# analysis. This function takes the product of either skilled_graph_hires or skilled_graph and
-	# a list of the start year, end year, and quarter that the analysis should be conducted on.
-	industries=set([row[2] for row in skill_data])
+	# analysis. This function takes the product of either education_graph_hires or eucation_graph 
+	# and a list of the start year, end year, and quarter that the analysis should be conducted on.
+	industries=set([row[2] for row in educ_data])
 	master=[]
 
 	for industry in industries:
 		wage_start=0
 		employment=0
-		for row in [row for row in skill_data if row[2]==industry]:
+		for row in [row for row in educ_data if row[2]==industry]:
 			if row[0]==dates[0]:
 				wage_start=wage_start+row[3]
 				employment=employment+row[5]
 
 			if row[0]==dates[0] and row[1]==dates[2]:
-				skill_start=row[4]
+				educ_start=row[4]
 			if row[0]==dates[1]  and row[1]==dates[2]:
-				skill_end=row[4]
+				educ_end=row[4]
 
 		# Wage is multiplied by .75 because it is currently the sum of four quarters. The base variable
 		# is months, so multiplying it by 3/4 averages it over quarters and then converts it from months
 		# to actual quarterly earnings. Employment is divided by 4 because it is also currently just the
-		# sum of 4 quarters. The headers for this should be: ['industry','start','end','skill_delta',
+		# sum of 4 quarters. The headers for this should be: ['industry','start','end','educ_delta',
 		# 'wages','emp']
-		master.append([industry,skill_start,skill_end,skill_end-skill_start,wage_start*.75,employment/4])
+		master.append([industry,educ_start,educ_end,educ_end-educ_start,wage_start*.75,employment/4])
 	return master
 
 
 def write(file,master):
-	# Once master has been created by skilled_graph2, this will write the data to 'file' with headers.
+	# Once master has been created by education_graph2, this will write the data to 'file' with headers.
 	with open(file,'wb') as csvfile:
 		writer=csv.writer(csvfile)
-		writer.writerow(['industry','start','end','skill_delta','wages','emp'])
+		writer.writerow(['industry','start','end','educ_delta','wages','emp'])
 		for row in master:
 			writer.writerow(row)
 
